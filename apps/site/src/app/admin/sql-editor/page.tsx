a649d6a126e4ec647ad529f655c391ee982895b6
Eugene Creevey <ec@altrp.org>
2026-01-05 09:52:36 +0000

    chore: fix line endings in various files

--- apps/site/src/app/admin/sql-editor/page.tsx
+++ apps/site/src/app/admin/sql-editor/page.tsx
@@ -9,3 +9,4 @@ export default function SqlEditorPage() {
   return <SqlEditorPageClient />
 }
 
+
--- apps/site/src/app/m/profile/page.tsx
+++ apps/site/src/app/m/profile/page.tsx
@@ -9,3 +9,4 @@ export default function ManagerProfilePage() {
   return <ManagerProfilePageClient />
 }
 
+
