a86bf95a7f97540824ac77a17d999701eff9c4cf
unknown <top506@yandex.ru>
2026-02-02 17:07:20 +0300

    updated roles list

--- apps/site/src/shared/seeds/system.ts
+++ apps/site/src/shared/seeds/system.ts
@@ -21,7 +21,28 @@ export const systemSeed = {
             is_system: 1,
             order: 100,
         },
-
+        {
+            uuid: "b2c3d4e5-40a6-42bb-9de1-b72d8e1c0940",
+            name: "tester",
+            title: "Тестер",
+            dataIn: {
+                auth_redirect_url: '/t',
+            },
+            description: "Роль для тестировщиков",
+            is_system: 1,
+            order: 200,
+        },
+        {
+            uuid: "c3d4e5f6-51b7-43cc-0ef2-c83e9f2d1051",
+            name: "Administrator",
+            title: "Администратор",
+            dataIn: {
+                auth_redirect_url: '/a',
+            },
+            description: "Роль администратора системы",
+            is_system: 1,
+            order: 10,
+        },
     ],
 
     taxonomy: [
